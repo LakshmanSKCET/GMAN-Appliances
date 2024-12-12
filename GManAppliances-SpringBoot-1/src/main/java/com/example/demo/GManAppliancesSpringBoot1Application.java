93604b0005035fae74865c5afcf1814fa58e3848
LakshmanSKCET <151318153+LakshmanSKCET@users.noreply.github.com>
2024-12-12 23:25:55 +0530

    Add files via upload

---
+++ GManAppliances-SpringBoot-1/src/main/java/com/example/demo/GManAppliancesSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GManAppliancesSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GManAppliancesSpringBoot1Application.class, args);
+	}
+
+}
